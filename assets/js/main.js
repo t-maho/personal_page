098ffda59fcda4ed06b924efb532a875373ce617
t-maho <thibault.maho@inria.fr>
2022-08-30 10:22:43 +0200

    index navbar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,14 @@
 			});*/
 
 })(jQuery);
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function navbar() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
